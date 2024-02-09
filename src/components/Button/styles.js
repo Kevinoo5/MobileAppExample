f69709ac91abbbc0a8efa26b8ae7f47fa0c7506a
Kevinoo5 <kevin.noormets@voco.ee>
2024-02-09 11:28:17 +0200

    Create splash screen

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        backgroundColor: "#4F63AC",
+        paddingVertical: 20,
+        paddingHorizontal: 8,
+        borderRadius: 8,
+    },
+    title: {
+        color: "white",
+        textAlign: "center",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+});
