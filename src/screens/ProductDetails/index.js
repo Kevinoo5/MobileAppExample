980d4b0be981ea153396cfa36744fdd702f126b7
Kevinoo5 <kevin.noormets@voco.ee>
2024-03-14 14:34:00 +0200

    Create product details screen

--- src/screens/ProductDetails/index.js
+++ src/screens/ProductDetails/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Image, Text, ScrollView, View, Pressable } from "react-native";
+import { Image, Text, ScrollView, View, Pressable, Linking } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "../../components/Button";
 import { styles } from "./styles";
+import ImageCarousel from "../../components/ImageCarousel";
 
 const ProductDetails = ({navigation, route}) => {
     const { product } = route.params || {}
@@ -11,10 +12,25 @@ const ProductDetails = ({navigation, route}) => {
         navigation.goBack();
     }
 
+    const onContact = () => {
+        // phone call
+        let phone = "phone number"
+        Linking.openURL(`tel:${phone}`)
+
+        // email
+        let email = "email"
+        Linking.openURL(`mailto:${email}`)
+    }
+
     return (
     <SafeAreaView style={styles.save}>
         <ScrollView>
-        <Image style={styles.image} source={{uri: product?.image}}/>
+        {product?.images?.length ? (
+                <ImageCarousel images={product.images} />
+            ) : (
+                <Image style={styles.image} source={{uri: product.image}} />
+            )}
+
         <View style={styles.content}>
             <Text style={styles.title}>{product?.title}</Text>
             <Text style={styles.price}>{product?.price}</Text>
@@ -29,7 +45,7 @@ const ProductDetails = ({navigation, route}) => {
             <Image style={styles.bookmark} source={require("../../assets/tabs/bookmark.png")}/>
             </Pressable>
 
-       <Button title="Contact Seller" />
+       <Button onPress={onContact} title="Contact Seller" />
         </View>
     </SafeAreaView>
     );
